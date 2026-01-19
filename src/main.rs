edeb58aa42161f45debd4e6b0c97d362634e82a4
tmpk <>
2026-01-19 14:53:37 -0800

    Board is blinking

--- src/main.rs
+++ src/main.rs
@@ -3,28 +3,22 @@
 
 use cortex_m_rt::entry;
 use embedded_hal::delay::DelayNs;
-use microbit::{
-    Board,
-    display::{self, blocking::Display},
-    gpio::DisplayPins,
-    hal::{self, timer},
-};
+use microbit::{Board, display::blocking::Display, hal::timer::Timer};
 use panic_halt as _;
-use rtt_target::{rprintln, rtt_init_print};
+// use rtt_target::{rprintln, rtt_init_print};
 
 const FPS: u32 = 10u32;
 
 #[entry]
 fn main() -> ! {
-    rtt_init_print!();
+    // rtt_init_print!();
 
     let board = Board::take().unwrap();
-    let mut timer1 = hal::Timer::new(board.TIMER1);
+    let mut timer1 = Timer::new(board.TIMER1);
     let mut display = Display::new(board.display_pins);
 
     let mut grid = [[0u8; 5]; 5];
     grid[0][1] = 1;
-    // let image = &BitImage::new(&grid);
 
     loop {
         display.show(&mut timer1, grid, 100);
