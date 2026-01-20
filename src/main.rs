2806fceb554fd5655b07fc26912e19e6c551cf67
tmpk <>
2026-01-19 16:19:33 -0800

    Switching to HAL RNG interface

--- src/main.rs
+++ src/main.rs
@@ -6,32 +6,38 @@ use life::*;
 
 use cortex_m_rt::entry;
 use embedded_hal::delay::DelayNs;
-use microbit::{Board, display::blocking::Display, hal::timer::Timer};
+use microbit::{Board, display::blocking::Display, hal::rng::Rng, hal::timer::Timer};
 use panic_halt as _;
-// use rtt_target::{rprintln, rtt_init_print};
+use rtt_target::{rprintln, rtt_init_print};
 
 const FPS: u32 = 10u32;
 
 #[entry]
 fn main() -> ! {
-    // rtt_init_print!();
+    rtt_init_print!();
 
     let board = Board::take().unwrap();
     let mut timer1 = Timer::new(board.TIMER1);
     let mut display = Display::new(board.display_pins);
-
+    let mut rng = Rng::new(board.RNG);
     /**
      * RNG
-     * rng_value: read rng
-     * rng_start: trigger generation
-     * rng_stop: stop generation
-     * rng_valrdy: event triggered when each rng is created
+     * rng_value: read rng, u8
+     * rng_start: trigger generation, bool
+     * rng_stop: stop generation, bool
+     * rng_valrdy: generated when each rng is created, bool
      */
-    let rng = board.RNG;
-    let rng_value = &rng.value;
-    let rng_start = &rng.tasks_start;
-    let rng_stop = &rng.tasks_stop;
-    let rng_valrdy = &rng.events_valrdy;
+    // let rng = board.RNG;
+    // let rng_value = &rng.value;
+    // let rng_start = &rng.tasks_start;
+    // let rng_stop = &rng.tasks_stop;
+    // let rng_valrdy = &rng.events_valrdy;
+
+    // // rng::tasks_start::TASKS_START
+    // let rng_start_val = rng::tasks_start::W::bits(&mut self, 1);
+    // rng_start.write();
+
+    rprintln!("{}", rng.random_u8());
 
     let mut grid = [[0u8; 5]; 5];
     for r in 0..5 {
