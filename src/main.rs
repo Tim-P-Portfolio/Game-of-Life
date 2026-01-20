73dbefac67667c9d993dbaed3c8c48031e0c8b63
tmpk <>
2026-01-19 16:23:06 -0800

    Generating random numbers

--- src/main.rs
+++ src/main.rs
@@ -20,28 +20,16 @@ fn main() -> ! {
     let mut timer1 = Timer::new(board.TIMER1);
     let mut display = Display::new(board.display_pins);
     let mut rng = Rng::new(board.RNG);
-    /**
-     * RNG
-     * rng_value: read rng, u8
-     * rng_start: trigger generation, bool
-     * rng_stop: stop generation, bool
-     * rng_valrdy: generated when each rng is created, bool
-     */
-    // let rng = board.RNG;
-    // let rng_value = &rng.value;
-    // let rng_start = &rng.tasks_start;
-    // let rng_stop = &rng.tasks_stop;
-    // let rng_valrdy = &rng.events_valrdy;
-
-    // // rng::tasks_start::TASKS_START
-    // let rng_start_val = rng::tasks_start::W::bits(&mut self, 1);
-    // rng_start.write();
 
     rprintln!("{}", rng.random_u8());
 
     let mut grid = [[0u8; 5]; 5];
-    for r in 0..5 {
-        grid[r][1] = 1
+    for c in 0..5 {
+        for r in 0..5 {
+            let num = rng.random_u8() / 255;
+            grid[r][c] = num;
+            rprintln!("{}", num);
+        }
     }
 
     // done, life
