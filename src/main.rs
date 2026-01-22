019dfd1b0b9a6e02f72609465146ca3c9fc486e7
tmpk <>
2026-01-22 14:11:41 -0800

    Not running, implementing stall buffer

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 #![no_main]
 #![no_std]
 
-mod life;
-use core::ops::Index;
+mod grid;
+use grid::*;
 
+mod life;
 use life::*;
 
 use embedded_hal::digital::InputPin;
@@ -19,127 +20,7 @@ use rtt_target::{rprint, rprintln, rtt_init_print};
 
 const USING_STALL: bool = cfg!(feature = "detect_stall");
 
-// Wether or not to display the text microbit
-const PRINT_MICROBIT: bool = cfg!(feature = "print_microbit");
-
-const MICROBIT: [&str; 7] = [
-    // Top
-    "╭───────────────────────╮",
-    "│          ▁▁▁          │",
-    "│         (● ●)         │",
-    "│          ▔▔▔          │",
-    // Bottom
-    "│                       │",
-    "│ ◯    ◯    ◯    ◯    ◯ │",
-    r#"╰/▔\__/▔\__/▔\__/▔\__/▔\╯"#,
-];
-
 const FPS: u32 = 10;
-
-// Grid for setting the state of the display leds
-#[derive(Copy, Clone)]
-struct Grid {
-    grid: [[u8; 5]; 5],
-}
-
-impl Grid {
-    fn new() -> Self {
-        // Initialize 5x5 grid with zeros
-        Self { grid: [[0; 5]; 5] }
-    }
-
-    // Flip all grid states
-    fn complement(&mut self) {
-        for r in 0..5 {
-            for c in 0..5 {
-                self.grid[r][c] = 1 - self.grid[r][c]
-            }
-        }
-    }
-
-    fn set(&mut self, grid: [[u8; 5]; 5]) {
-        for r in 0..5 {
-            for c in 0..5 {
-                self.grid[r][c] = grid[r][c]
-            }
-        }
-    }
-
-    fn generate_random_grid(&mut self, rng: &mut Rng) {
-        // Display first 3 lines of the text based microbit in the terminal
-        if PRINT_MICROBIT {
-            for i in 0..4 {
-                rprintln!("\r{}", MICROBIT[i]);
-            }
-        }
-        // Loop through grid cells
-        for c in 0..5 {
-            // Print edge and spacing for microbit
-            if PRINT_MICROBIT {
-                rprint!("\r│      ")
-            }
-            for r in 0..5 {
-                // Set led state to random value, 0-127: 0, 128-255: 1
-                let num = if rng.random_u8() > 127 { 1 } else { 0 };
-                // Set microbit
-                self.grid[c][r] = num;
-                // Print text version of starting state of leds
-                if PRINT_MICROBIT {
-                    rprint!("{}", if num == 1 { " ▮" } else { " ▯" })
-                }
-            }
-            // Print edge and spacing for microbit
-            if PRINT_MICROBIT {
-                rprint!("       │\n");
-            }
-        }
-
-        // Display last 3 lines of the microbit in the terminal
-        if PRINT_MICROBIT {
-            for i in 5..7 {
-                rprintln!("\r{}", MICROBIT[i]);
-            }
-        }
-    }
-}
-
-const BUFFER_SIZE: usize = 3;
-
-/*
- * Grid Buffer
- *
- * grids: list of grids
- * top: most recent grid
- *
- * Insert new grid at top+1
- * Set top to new grid
- */
-struct GridBuffer {
-    grids: [Grid; BUFFER_SIZE],
-    top: usize,
-    repeat: u8,
-}
-impl GridBuffer {
-    fn new() -> Self {
-        Self {
-            grids: [Grid::new(); BUFFER_SIZE],
-            top: 0,
-            repeat: 0,
-        }
-    }
-    fn set(&mut self, grid: Grid) {
-        // if grid new = grid top+1 set repeat counter else reset repeat
-        // if repeat counter > Buffer size return T/F
-
-        self.top = if self.top < BUFFER_SIZE {
-            self.top + 1
-        } else {
-            0
-        };
-
-        self.grids[self.top] = grid;
-    }
-}
 
 // State of the game
 enum GameState {
@@ -179,8 +60,6 @@ fn main() -> ! {
 
     // Initialize the counters
     let mut b_btn_frame_count = 5;
-    let mut stall_frame_count = 5;
-    // let mut done_frame_count = 1;
 
     // Initialize state to start as random grid
     let mut state = GameState::Randomize;
@@ -218,12 +97,7 @@ fn main() -> ! {
                 //     GameState::Done
                 } else {
                     if USING_STALL {
-                        if past_grids.grids[0].grid == grid.grid {
-                            stall_frame_count += 1
-                        } else {
-                            past_grids.grids[0].set(grid.grid);
-                            stall_frame_count = 1
-                        }
+                        past_grids.set(grid);
                     }
                     // Run life proceedure on grid
                     life(&mut grid.grid);
